2740ad6ea840597879a6bb047b1a3eb760f4e14f
Yanglin Zhao <yz1876@gmail.com>
2015-04-26 22:34:24 -0700

    Add hello world of react component

---
+++ src/_assets/scripts/components/songs.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Songs = React.createClass({
+  render: function() {
+    return (
+      <div className="songs">
+        Songs component
+      </div>
+    );
+  }
+});
+
+module.exports = Songs;
--- src/_assets/scripts/main.js
+++ src/_assets/scripts/main.js
@@ -1,1 +1,7 @@
-/* _assets/js/main.js */
+var React = require('react');
+var Songs = require('./components/songs.js');
+
+React.render(
+  <Songs />,
+  document.getElementById('app')
+);
