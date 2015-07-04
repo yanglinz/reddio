dd19999b48b57015598ecfaf0cb7c05b848d6280
Yanglin Zhao <yz1876@gmail.com>
2015-07-04 16:30:27 -0700

    Delete backup scripts

--- src/_assets/scripts.back/__tests__/main-test.js
+++
@@ -1,7 +0,0 @@
-var expect = require('expect');
-
-describe('Dummy test', function describeDummyTests() {
-  it('Should pass', function dummyTestCase() {
-    expect(1).toEqual(1);
-  });
-});
--- src/_assets/scripts.back/lib/logger.js
+++
@@ -1,7 +0,0 @@
-function logError(message) {
-  /* eslint-disable */
-  console.error(message);
-  /* eslint-enable */
-}
-
-export {logError};
