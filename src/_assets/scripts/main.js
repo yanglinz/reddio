5bb0c3b42fe9d51aad05ff8444e61edffefe22ad
Yanglin Zhao <yz1876@gmail.com>
2015-07-18 17:48:50 -0700

    Move and refactor components folder

--- src/_assets/scripts/main.js
+++ src/_assets/scripts/main.js
@@ -1,5 +1,43 @@
 import React from 'react';
 import AppContainer from './components/index.js';
+import Masthead from './app/masthead.js';
+import RedditController from './controllers/reddit.controller.js';
+import PlayerController from './controllers/player.controller.js';
+import { appStateStream } from '../core/state.js';
+
+class AppContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    /**
+     * appStateStream is an observable stream of all appState changes.
+     * Trigger a re-render of the AppContainer component by calling setState
+     */
+    appStateStream.subscribe(function renderOnStateChange() {
+      this.setState({});
+    }.bind(this));
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <header className="app-header">
+          <Masthead />
+        </header>
+
+        <main className="app-content">
+          <RedditController />
+        </main>
+
+        <footer className="app-footer">
+          <PlayerController />
+        </footer>
+      </div>
+    );
+  }
+}
 
 React.render(
   <AppContainer />,
