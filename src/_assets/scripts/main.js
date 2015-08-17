b73998a99072c11e5cc7d17e82c72c46e8cd36f5
Yanglin Zhao <yz1876@gmail.com>
2015-08-16 22:56:16 -0700

    Create an entry point for the app.js

--- src/_assets/scripts/main.js
+++ src/_assets/scripts/main.js
@@ -1,44 +1,4 @@
 import React from 'react';
-import Masthead from './reddit/views/masthead.js';
-import RedditController from './reddit/views/controller.js';
-import PlayerController from './player/views/controller.js';
-import { appStateStream } from './core/state.js';
+import App from './core/app.js';
 
-class AppContainer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  componentDidMount() {
-    /**
-     * appStateStream is an observable stream of all appState changes.
-     * Trigger a re-render of the AppContainer component by calling setState
-     */
-    appStateStream.subscribe(function renderOnStateChange() {
-      this.setState({});
-    }.bind(this));
-  }
-
-  render() {
-    return (
-      <div className="app-container">
-        <header className="app-header">
-          <Masthead />
-        </header>
-
-        <main className="app-content">
-          <RedditController />
-        </main>
-
-        <footer className="app-footer">
-          <PlayerController />
-        </footer>
-      </div>
-    );
-  }
-}
-
-React.render(
-  <AppContainer />,
-  document.getElementById('app')
-);
+React.render(<App/>, document.getElementById('app'));
