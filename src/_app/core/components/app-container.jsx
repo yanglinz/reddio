782851aa1d2debe529de0bb75f35a4566556d483
Yanglin Zhao <yz1876@gmail.com>
2015-08-23 15:06:20 -0700

    Use material ui app bar for header

--- src/_app/core/components/app-container.jsx
+++ src/_app/core/components/app-container.jsx
@@ -1,10 +1,14 @@
 import React, { Component, PropTypes } from 'react';
+import { AppBar } from 'material-ui';
+import materialUI from 'core/components/decorators/material-ui.js';
+import './app.css';
 
+@materialUI
 class AppContainer extends Component {
   render() {
     return (
       <div>
-        <h1>App container</h1>
+        <AppBar title="reddio" />
         {this.props.children}
       </div>
     );
