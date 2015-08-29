0a8a533b3e4b42c52668aac89935bf5e47ba7c20
Yanglin Zhao <yz1876@gmail.com>
2015-08-29 01:39:05 -0700

    Use es6 for deploy script

--- build/gulp/deploy.js
+++ build/gulp/deploy.js
@@ -1,23 +1,23 @@
-var path = require('path');
-var assert = require('assert');
-var gulp = require('gulp');
-var utilities = require('../utilities.js');
-var environment = require('../environment.js');
+import path from 'path';
+import assert from 'assert';
+import gulp from 'gulp';
+import { spawnChildProcess } from '../utilities.js';
+import settings from '../../settings.js';
 
-gulp.task('deploy:surge', function(callback) {
-  assert(environment.SURGE_DOMAIN);
-  if (environment.TRAVIS) {
-    assert(environment.SURGE_TOKEN);
+gulp.task('deploy:surge', (callback) => {
+  assert(settings.SURGE_DOMAIN);
+  if (settings.IS_TRAVIS) {
+    assert(settings.SURGE_TOKEN);
   }
 
-  var surgeCli = path.resolve(__dirname, '../../node_modules/.bin/surge');
-  var targetDir = path.resolve(__dirname, '../../dist');
-  var command = [
+  const surgeCli = path.resolve(__dirname, '../../node_modules/.bin/surge');
+  const targetDir = path.resolve(__dirname, '../../dist');
+  const command = [
     surgeCli,
     '--project',
     targetDir,
     '--domain',
-    environment.SURGE_DOMAIN
+    settings.SURGE_DOMAIN
   ].join(' ');
-  utilities.spawnChildProcess(command, {callback: callback});
+  spawnChildProcess(command, {callback: callback});
 });
--- settings.js
+++ settings.js
@@ -14,7 +14,10 @@ const IS_PROD = any([
 const settings = {
   ENVIRONMENT: ENVIRONMENT,
   IS_PROD: IS_PROD,
-  IS_LOCAL: !IS_PROD
+  IS_LOCAL: !IS_PROD,
+  IS_TRAVIS: process.env.TRAVIS,
+  SURGE_DOMAIN: process.env.SURGE_DOMAIN,
+  SURGE_TOKEN: process.env.SURGE_TOKEN
 };
 
 export default settings;
