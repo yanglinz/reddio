6c377312738d05e834c306737ffee62e7ec8840a
Yanglin Zhao <yz1876@gmail.com>
2015-09-22 22:59:20 -0700

    Update eslint

--- src/_app/core/components/higher-order/router.jsx
+++ src/_app/core/components/higher-order/router.jsx
@@ -5,12 +5,12 @@ import { Navigation, State } from 'react-router';
 const RouterComponent = React.createClass({
   mixins: [Navigation, State],
 
-  getCurrentRouteName: function() {
+  getCurrentRouteName: function getCurrentRouteName() {
     const path = window.location.href;
     return last(path.split('/'));
   },
 
-  render: function() {
+  render: function render() {
     return (
       <div></div>
     );
