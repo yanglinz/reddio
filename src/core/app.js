512c76509712c561075ba92567b69ca434a9da39
Yanglin Zhao <yz1876@gmail.com>
2015-11-21 23:13:05 -0800

    Render hello world

---
+++ src/core/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+module.exports = function renderApp() {
+  return (
+    <div className="app">
+      <h1>App</h1>
+    </div>
+  );
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './core/app.js';
 import { IS_LOCAL } from './config.js';
 
 require('./main.scss');
+
+const mountNode = document.getElementById('app-mount');
+ReactDOM.render(<App />, mountNode);
 
 if (IS_LOCAL && module.hot) {
   module.hot.accept();
