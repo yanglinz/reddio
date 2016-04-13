26cfe3b7d84cc82efd84bf4ec1558bfe5ce5c7de
Yanglin Zhao <yz1876@gmail.com>
2016-04-13 07:21:57 -0400

    Add environmental config test

--- src/app/core/__tests__/config-test.js
+++ src/app/core/__tests__/config-test.js
@@ -1,7 +1,15 @@
+import _ from 'lodash';
 import { expect } from 'chai';
 
+import config from '../config.js';
+
 describe('config test', () => {
-  it('should pass', () => {
-    expect(1).to.equal(1);
+  it('should have the expected environmental variables', () => {
+    expect(_.keys(config)).to.deep.equal([
+      'NODE_ENV',
+      'ENVIRONMENT',
+      'IS_PROD',
+      'SEGMENT_API_KEY'
+    ]);
   });
 });
