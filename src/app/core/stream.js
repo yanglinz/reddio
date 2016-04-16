5ace07f78b8d3349a0c490aa418ba89f32a0adaf
Yanglin Zhao <yz1876@gmail.com>
2016-04-16 17:10:47 -0400

    Add source types

--- src/app/core/stream.js
+++ src/app/core/stream.js
@@ -4,6 +4,14 @@ import rx from 'rxjs';
 import { playerStreamReducer } from 'player/stream.js';
 
 /**
+ * Source stream types
+ */
+export const SOURCE_TYPES = {
+  EVENT: 'EVENT',
+  COMMAND: 'COMMAND'
+}
+
+/**
  * Create singleton source and sink subject streams
  * Source stream represents incoming events and commands
  * Sink stream represents filtered actions that will be given to redux actions
@@ -12,10 +20,19 @@ const _source$ = rx.Subject();
 const _sink$ = rx.Subject();
 
 /**
- * Dispatch an event or command to the source stream
+ * Dispatch an event to the source stream
  */
-export function dispatchToStream(source$ = _source$, payload) {
-  source$.onNext(payload);
+export function dispatchEvent(payload, stream = _source$) {
+  payload = assign(payload, { __type: SOURCE_TYPES.EVENT });
+  stream.onNext(payload);
+}
+
+/**
+ * Dispatch a command to source stream
+ */
+export function dispatchCommand(payload, stream = _source$) {
+  payload = assign(payload, { __type: SOURCE_TYPES.COMMAND });
+  stream.onNext(payload);
 }
 
 const defaultStreamReducers = [
