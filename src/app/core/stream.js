4050c9edf53c4629c70bd72b954d95f5d3abbd47
Yanglin Zhao <yz1876@gmail.com>
2016-04-16 09:46:17 -0400

    Clean rx functions

--- src/app/core/stream.js
+++ src/app/core/stream.js
@@ -4,15 +4,15 @@ import Rx from 'rxjs';
 import { playerStreamReducer } from 'player/stream.js';
 
 /**
- * Define singleton streams.
- * Source stream represents incoming actions
+ * Create singleton source and sink subject streams
+ * Source stream represents incoming events and commands
  * Sink stream represents filtered actions that will be given to redux actions
  */
 const _source$ = Rx.Subject();
 const _sink$ = Rx.Subject();
 
 /**
- * Define a function to dispatch an event to the source stream.
+ * Dispatch an event or command to the source stream
  */
 export function dispatchToStream(source$ = _source$, payload) {
   source$.onNext(payload);
@@ -23,11 +23,10 @@ const defaultStreamReducers = [
 ];
 
 /**
- * Apply a set of stream reducing functions to the source stream
- * @param {array} streamReducers - A list of reducing functions to apply to source stream
- * @returns {object} - Reduced stream
+ * Apply a set of reducing functions to the source stream
+ * These transforming functions will ultimately map a single source stream to a single sink stream
  */
-export function reduceStream(source$ = _source$, streamReducers = defaultStreamReducers) {
+export function applyReducers(source$ = _source$, streamReducers = defaultStreamReducers) {
   const streams = _.map(streamReducers, (streamReducer) => streamReducer(source$));
   return Rx.Observable.merge(streams);
 }
@@ -35,14 +34,6 @@ export function reduceStream(source$ = _source$, streamReducers = defaultStreamR
 /**
  * Register stream to dispatch sink stream actions to redux store
  */
-export function registerStream(source$ = _source$, sink$ = _sink$) {
+export function registerStreams(source$ = _source$, sink$ = _sink$) {
   const sink$ = reduceStream(source$, defaultStreamReducers);
-}
-
-export function sourceStream() {
-  return _source$;
-}
-
-export function sinkStream() {
-  return _sink$;
 }
