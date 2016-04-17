2741fc74fe537c9f041ef544a5fe394699cd7295
Yanglin Zhao <yz1876@gmail.com>
2016-04-16 23:16:54 -0400

    Use stateless functions

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello World</h1>
-    );
-  }
+function App() {
+  return (
+    <h1>Hello World</h1>
+  );
 }
 
 const mountNode = document.getElementById('app-mount');
