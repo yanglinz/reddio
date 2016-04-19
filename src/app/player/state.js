20f0fe554d2723bbb33997efa0b8480414e7fca3
Yanglin Zhao <yz1876@gmail.com>
2016-04-18 22:22:24 -0400

    Add placeholder reducer functions

--- src/app/player/state.js
+++ src/app/player/state.js
@@ -16,14 +16,42 @@ export function initialState() {
   };
 }
 
+function reduceSetState(state, action) {
+  return state;
+}
+
+function reduceSetQueue(state, action) {
+  return state;
+}
+
+function reduceSetCurrentSong(state, action) {
+  return state;
+}
+
+function reduceSetNextSong(state, action) {
+  return state;
+}
+
+function reduceSetPreviousSong(state, action) {
+  return state;
+}
+
+function reduceShuffle(state, action) {
+  return state;
+}
+
+function reduceUnshuffle(state, action) {
+  return state;
+}
+
 const reducerByAction = {
-  [PLAYER_STATE_TRANSITIONS.SET_STATE]: _.identity,
-  [PLAYER_STATE_TRANSITIONS.SET_QUEUE]: _.identity,
-  [PLAYER_STATE_TRANSITIONS.SET_CURRENT_SONG]: _.identity,
-  [PLAYER_STATE_TRANSITIONS.SET_NEXT_SONG]: _.identity,
-  [PLAYER_STATE_TRANSITIONS.SET_PREVIOUS_SONG]: _.identity,
-  [PLAYER_STATE_TRANSITIONS.SHUFFLE]: _.identity,
-  [PLAYER_STATE_TRANSITIONS.UNSHUFFLE]: _.identity
+  [PLAYER_STATE_TRANSITIONS.SET_STATE]: reduceSetState,
+  [PLAYER_STATE_TRANSITIONS.SET_QUEUE]: reduceSetQueue,
+  [PLAYER_STATE_TRANSITIONS.SET_CURRENT_SONG]: reduceSetCurrentSong,
+  [PLAYER_STATE_TRANSITIONS.SET_NEXT_SONG]: reduceSetNextSong,
+  [PLAYER_STATE_TRANSITIONS.SET_PREVIOUS_SONG]: reduceSetPreviousSong,
+  [PLAYER_STATE_TRANSITIONS.SHUFFLE]: reduceShuffle,
+  [PLAYER_STATE_TRANSITIONS.UNSHUFFLE]: reduceUnshuffle
 };
 
 export function playerReducer(state = initialState(), action) {
