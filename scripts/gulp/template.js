d3d59663dee7b03320f6223cb3069df9543941fa
Yanglin Zhao <yz1876@gmail.com>
2016-05-06 08:59:12 -0400

    Include newrelic snippet in produciton

--- scripts/gulp/template.js
+++ scripts/gulp/template.js
@@ -3,19 +3,22 @@ import data from 'gulp-data';
 import nunjucks from 'gulp-nunjucks';
 
 import { webpackAssets, webpackWatchAssets } from '../../.config/webpack.js';
+import { browserConfigs } from '../../config.js';
 
 const templates = ['src/**/*.html'];
 
 function buildTemplate() {
   return gulp.src(templates)
-    .pipe(data(() => webpackAssets()))
+    .pipe(data(webpackAssets))
+    .pipe(data(browserConfigs))
     .pipe(nunjucks.compile())
     .pipe(gulp.dest('dist'));
 }
 
 function buildWatchTemplate() {
   return gulp.src(templates)
-    .pipe(data(() => webpackWatchAssets()))
+    .pipe(data(webpackWatchAssets))
+    .pipe(data(browserConfigs))
     .pipe(nunjucks.compile())
     .pipe(gulp.dest('dist'));
 }
