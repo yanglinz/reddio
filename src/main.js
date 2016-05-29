7a5df19f66ec0a6b0dc65e9fff415dd83b281b40
Yanglin Zhao <yz1876@gmail.com>
2016-05-29 09:37:44 -0400

    Fix sentry and segment settings on local builds

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import './core/entry.js';
-import './core/styles/main.scss';
-
 import 'analytics/segment.js';
 import 'analytics/sentry.js';
-import './core/app.js';
+
+import './core/entry.js';
+import './core/styles/main.scss';
 
 if (module.hot) {
   module.hot.accept();
