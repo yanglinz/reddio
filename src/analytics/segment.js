f3f9e73ae32842e44606b878a305e233d0c26b2a
Yanglin Zhao <yanglinz@users.noreply.github.com>
2016-05-29 10:02:36 -0400

    Fix sentry and segment settings on local builds (#20)

--- src/analytics/segment.js
+++ src/analytics/segment.js
@@ -1,10 +1,15 @@
+import _ from 'lodash';
 import analyticsLoader from 'analytics.js-loader';
 
 import settings from 'core/settings.js';
 
-const analytics = analyticsLoader({
-  writeKey: settings.SEGMENT_API_KEY
-});
+const analyticsShim = {
+  track: _.noop
+};
+
+const analytics = settings.IS_PROD
+  ? analyticsLoader({ writeKey: settings.SEGMENT_API_KEY })
+  : analyticsShim;
 
 export function track(eventName, params = {}) {
   analytics.track(eventName, params);
--- src/analytics/sentry.js
+++ src/analytics/sentry.js
@@ -3,5 +3,5 @@ import Raven from 'raven-js';
 import settings from 'core/settings.js';
 
 if (settings.IS_PROD) {
-  Raven.settings(settings.SENTRY_DSN).install();
+  Raven.config(settings.SENTRY_DSN).install();
 }
