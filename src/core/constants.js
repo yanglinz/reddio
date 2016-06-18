7843fbf3cba9f917915702c95ad78d1473e9d9bf
Yanglin Zhao <yz1876@gmail.com>
2016-06-17 22:52:09 -0400

    Add core constants

---
+++ src/core/constants.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+
+/**
+ * Define core application events
+ */
+const _CORE_EVENTS = [
+  'SAGA_INIT'
+];
+
+export const CORE_EVENTS = _.zipObject(_CORE_EVENTS, _CORE_EVENTS);
