284a9db80bcaeb6237ad6aa25f3c7be5b5f2447d
Yanglin Zhao <yz1876@gmail.com>
2016-08-07 15:49:54 -0400

    Split entry and routes

---
+++ src/core/routes.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, hashHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import store from 'core/state.js';
+
+function Root(props) {
+  return (
+    <div className="root">
+      <h1>Reddio</h1>
+      {props.children}
+    </div>
+  );
+}
+
+Root.propTypes = {
+  children: React.PropTypes.element
+};
+
+function history() {
+  return syncHistoryWithStore(hashHistory, store);
+}
+
+function AppRoutes() {
+  return (
+    <Provider store={store}>
+      <Router history={history()}>
+        <Route path="/" component={Root} />
+      </Router>
+    </Provider>
+  );
+}
+
+export default AppRoutes;
