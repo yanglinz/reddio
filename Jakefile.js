069d66891dcbe3710ff6630c6ca3afdefe29a8d1
Yanglin Zhao <yanglinz@users.noreply.github.com>
2016-09-25 15:55:07 -0400

    Upgrade dependencies (#59)
    * Upgrade dependencies
    
    * Remove one-line jake commands
    
    * Silence linter

--- Jakefile.js
+++ Jakefile.js
@@ -5,22 +5,11 @@ const env = require('./environment');
 const { jake, task, desc, complete } = global;
 
 const BIN = path.join(__dirname, 'node_modules/.bin');
-const ESLINT = path.join(BIN, 'eslint');
 const FIREBASE = path.join(BIN, 'firebase');
-const KARMA = path.join(BIN, 'karma');
-const PROTRACTOR = path.join(BIN, 'protractor');
 const WEBPACK = path.join(BIN, 'webpack');
 const WEBPACK_DEV_SERVER = path.join(BIN, 'webpack-dev-server');
 
 const execOptions = { interactive: true, printStdout: true, printStderr: true };
-
-desc('Lint code for style errors');
-task('lint', [], () => {
-  const cmds = [
-    `${ESLINT} .`
-  ];
-  jake.exec(cmds, execOptions, complete);
-});
 
 desc('Build deploy artifact');
 task('build', [], () => {
@@ -39,35 +28,9 @@ task('watch', [], () => {
   jake.exec(cmds, execOptions, complete);
 });
 
-task('test', [], () => {
-  const cmds = [
-    `${KARMA} start --single-run`
-  ];
-  jake.exec(cmds, execOptions, complete);
-});
-
-task('test-watch', [], () => {
-  const cmds = [
-    `${KARMA} start`
-  ];
-  jake.exec(cmds, execOptions, complete);
-});
-
-task('test-e2e', [], () => {
-  const cmds = [
-    `${PROTRACTOR} protractor.conf.js`
-  ];
-  jake.exec(cmds, execOptions, complete);
-});
-
 task('deploy', [], () => {
   const cmds = [
     `${FIREBASE} deploy --token ${env.FIREBASE_TOKEN}`
   ];
   jake.exec(cmds, execOptions, complete);
-});
-
-desc('Remove build artifacts and temporary files');
-task('clean', [], () => {
-  jake.rmRf('dist');
 });
