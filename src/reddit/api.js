f59b6f4fb53aa3f8546c3a3aa0a6e681cb668070
Yanglin Zhao <yz1876@gmail.com>
2016-10-01 10:32:44 -0400

    Fix insecure reddit url

--- src/reddit/api.js
+++ src/reddit/api.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 
+import settings from 'core/settings';
+
 export function getListing(pathname, query) {
   const req = {
     method: 'GET',
     params: query
   };
-  const url = `http://www.reddit.com/${_.trimEnd(pathname, '/')}/.json`;
+  const url = `${settings.REDDIT_URL}/${_.trimEnd(pathname, '/')}/.json`;
   return fetch(url, req).then(res => res.json());
 }
