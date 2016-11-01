4b03f6aeac567a3efc22d6aac9930e3acb8d0971
Yanglin Zhao <yz1876@gmail.com>
2016-10-31 21:05:38 -0400

    Better iframe load actions

--- src/player/saga.js
+++ src/player/saga.js
@@ -1,10 +1,17 @@
 import { takeLatest } from 'redux-saga';
 import { call, fork, put } from 'redux-saga/effects';
 
+import { PLAYER_ACTIONS } from 'player/constants';
 import { load } from 'player/controls';
 
 export function* initializePlayer() {
-  yield call(load);
+  yield put({ type: PLAYER_ACTIONS.LOAD_IFRAME });
+  try {
+    yield call(load);
+    yield put({ type: PLAYER_ACTIONS.LOAD_IFRAME_DONE });
+  } catch (e) {
+    yield put({ type: PLAYER_ACTIONS.LOAD_IFRAME_FAIL });
+  }
 }
 
 export function* playerSaga() {
