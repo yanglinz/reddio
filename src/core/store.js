07623948c0e67321c03bcc774d3cc84e30a1f92e
Yanglin Zhao <yz1876@gmail.com>
2016-11-02 23:17:19 -0400

    Add dummy epics

--- src/core/store.js
+++ src/core/store.js
@@ -2,9 +2,11 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
 import createSaga from 'redux-saga';
+import { createEpicMiddleware } from 'redux-observable';
 
 import { rootReducer } from 'core/reducer';
 import { rootSaga } from 'core/saga';
+import rootEpic from 'core/epic';
 import settings from 'core/settings';
 
 export function isLoggerEnabled() {
@@ -19,7 +21,12 @@ export function configureStore(initialState) {
     collapsed: true,
   });
   const sagaMiddleware = createSaga();
-  const middleware = applyMiddleware(thunk, sagaMiddleware, loggerMiddleware);
+  const middleware = applyMiddleware(
+    thunk,
+    sagaMiddleware,
+    createEpicMiddleware(rootEpic),
+    loggerMiddleware,
+  );
   const store = createStore(rootReducer(), initialState, middleware);
   sagaMiddleware.run(rootSaga);
   return store;
---
+++ src/reddit/epic.js
@@ -0,0 +1,7 @@
+import Rx from 'rxjs/Rx';
+
+export default function redditEpic() {
+  return Rx.Observable
+    .interval(2500)
+    .map(() => ({ type: 'REDDIT_EPIC' }));
+}
