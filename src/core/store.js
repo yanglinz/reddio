476c26743509405531d0cdb5dda6fd15fc3f166b
Yanglin Zhao <yz1876@gmail.com>
2016-11-05 00:19:34 -0400

    Disable saga

--- src/core/store.js
+++ src/core/store.js
@@ -5,7 +5,6 @@ import createSaga from 'redux-saga';
 import { createEpicMiddleware } from 'redux-observable';
 
 import { rootReducer } from 'core/reducer';
-import { rootSaga } from 'core/saga';
 import rootEpic from 'core/epic';
 import settings from 'core/settings';
 
@@ -20,15 +19,12 @@ export function configureStore(initialState) {
     predicate: isLoggerEnabled,
     collapsed: true,
   });
-  const sagaMiddleware = createSaga();
   const middleware = applyMiddleware(
     thunk,
-    sagaMiddleware,
     createEpicMiddleware(rootEpic),
     loggerMiddleware,
   );
   const store = createStore(rootReducer(), initialState, middleware);
-  sagaMiddleware.run(rootSaga);
   return store;
 }
 
