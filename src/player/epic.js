5ae608937bbce6f9b99bc01b931e287727987b4b
Yanglin Zhao <yz1876@gmail.com>
2016-11-05 20:30:47 -0500

    Add player epic

--- src/player/epic.js
+++ src/player/epic.js
@@ -1,6 +1,43 @@
 import Rx from 'rxjs/Rx';
+import { combineEpics } from 'redux-observable';
 
-export default function redditEpic() {
-  return Rx.Observable
-    .of({ type: 'INITIALIZED_EPIC' });
+import { REDDIT_ACTIONS } from 'reddit/constants';
+import { PLAYER_ACTIONS } from 'player/constants';
+import { load, listen, play, pause } from 'player/controls';
+
+export function loadIframeEpic() {
+  const init$ = Rx.Observable
+    .of({ type: PLAYER_ACTIONS.LOAD_IFRAME });
+  const load$ = Rx.Observable.fromPromise(load())
+    .map(() => ({ type: PLAYER_ACTIONS.LOAD_IFRAME_DONE }))
+    .catch(() => ({ type: PLAYER_ACTIONS.LOAD_IFRAME_FAIL }));
+  return init$.concat(load$);
 }
+
+export function playEpic(actions$) {
+  return actions$
+    .ofType(REDDIT_ACTIONS.PLAY_POST)
+    .map((action) => {
+      const { payload } = action;
+      const { url } = payload.post.data;
+      play(url);
+      return { type: PLAYER_ACTIONS.PLAYING, payload };
+    });
+}
+
+export function pauseEpic(actions$) {
+  return actions$
+    .ofType(PLAYER_ACTIONS.PAUSE_PLAYER)
+    .map((action) => {
+      pause();
+      return { type: PLAYER_ACTIONS.PAUSING };
+    });
+}
+
+const redditEpic = combineEpics(
+  loadIframeEpic,
+  playEpic,
+  pauseEpic,
+);
+
+export default redditEpic;
