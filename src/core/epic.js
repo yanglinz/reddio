e5b1823d0eb032dc858e9620f0475512f09f76b9
Yanglin Zhao <yz1876@gmail.com>
2016-11-05 20:30:56 -0500

    Remove core initialize epic

--- src/core/epic.js
+++ src/core/epic.js
@@ -4,13 +4,7 @@ import { combineEpics } from 'redux-observable';
 import redditEpic from 'reddit/epic';
 import playerEpic from 'player/epic';
 
-function initializeEpic() {
-  return Rx.Observable
-    .of({ type: 'EPIC_INITIALIZED' });
-}
-
 const rootEpic = combineEpics(
-  initializeEpic,
   redditEpic,
   playerEpic,
 );
