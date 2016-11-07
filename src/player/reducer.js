5b78c095d3e8c0f8bd2d182302b97d4ba559c6f9
Yanglin Zhao <yz1876@gmail.com>
2016-11-06 21:03:24 -0500

    Clean up reducers

--- src/player/reducer.js
+++ src/player/reducer.js
@@ -1,13 +1,16 @@
 import _ from 'lodash';
 
-import { PLAYER_ACTIONS, PLAYER_STATES } from 'player/constants';
+import { PLAYER_TARGETS, PLAYER_STATES, PLAYER_ACTIONS } from 'player/constants';
 
 /**
  * Get initial player state
  */
 export function initialState() {
   return {
-    state: PLAYER_STATES.LOADING,
+    currentState: {
+      [PLAYER_TARGETS.YOUTUBE]: PLAYER_STATES.LOADING,
+      [PLAYER_TARGETS.SOUNDCLOUD]: PLAYER_STATES.LOADING,
+    },
     songs: {},
     currentSong: null,
     queue: [],
@@ -16,47 +19,22 @@ export function initialState() {
   };
 }
 
+function reduceSetReady(state, action) {
+  const currentState = {
+    [PLAYER_TARGETS.YOUTUBE]: PLAYER_STATES.LOADED,
+    [PLAYER_TARGETS.SOUNDCLOUD]: PLAYER_STATES.LOADED,
+  };
+  return _.assign({}, state, { currentState });
+}
+
 function reduceSetState(state, action) {
   const { payload } = action;
   return _.assign({}, state, { state: payload.state });
 }
 
-function reduceSetQueue(state, action) {
-  const { payload } = action;
-  _.noop(payload);
-  return state;
-}
-
-function reduceSetCurrentSong(state, action) {
-  const { payload } = action;
-  _.noop(payload);
-  return state;
-}
-
-function reduceSetNextSong(state) {
-  return state;
-}
-
-function reduceSetPreviousSong(state) {
-  return state;
-}
-
-function reduceShuffle(state) {
-  return state;
-}
-
-function reduceUnshuffle(state) {
-  return state;
-}
-
 const reducerByAction = {
-  [PLAYER_ACTIONS.SET_STATE]: reduceSetState,
-  [PLAYER_ACTIONS.SET_QUEUE]: reduceSetQueue,
-  [PLAYER_ACTIONS.SET_CURRENT_SONG]: reduceSetCurrentSong,
-  [PLAYER_ACTIONS.SET_NEXT_SONG]: reduceSetNextSong,
-  [PLAYER_ACTIONS.SET_PREVIOUS_SONG]: reduceSetPreviousSong,
-  [PLAYER_ACTIONS.SHUFFLE]: reduceShuffle,
-  [PLAYER_ACTIONS.UNSHUFFLE]: reduceUnshuffle,
+  [PLAYER_ACTIONS.LOAD_IFRAME_DONE]: reduceSetReady,
+  [PLAYER_ACTIONS.ON_EVENT]: reduceSetState,
 };
 
 export function playerReducer(state = initialState(), action) {
