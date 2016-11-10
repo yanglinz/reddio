66c0f88f17813900b96e0da6236e75ee3adb3c6e
Yanglin Zhao <yz1876@gmail.com>
2016-11-09 20:59:12 -0500

    Add more player styling

--- src/player/components/player.jsx
+++ src/player/components/player.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-import * as actions from 'player/actions';
-
+import Controls from './controls';
 import './player.scss';
 
-function stateToProps() {
-  return {};
-}
-
 export function Player(props) {
-  const { dispatch } = props;
-  const pauseCommand = dispatch.bind(null, actions.pauseCommand());
   return (
-    <div className="player">
-      <button>play</button>
-      <button onClick={pauseCommand}>pause</button>
+    <div className="Player">
+      <Controls {...props} />
     </div>
   );
 }
 
-Player.propTypes = {
-  dispatch: React.PropTypes.func,
-};
-
-module.exports = connect(stateToProps)(Player);
+module.exports = Player;
