c4fbfe4d458f57b094527ba1a3b3e14cfda938f8
Yanglin Zhao <yz1876@gmail.com>
2016-11-12 16:44:23 -0500

    State constants

--- src/state/__tests__/constants-test.js
+++ src/state/__tests__/constants-test.js
@@ -1,14 +1,26 @@
 import _ from 'lodash';
 import { expect } from 'chai';
 
-import { EVENTS } from '../constants';
+import { EVENTS, PLAYER_STATES } from '../constants';
 
 describe('state constants', () => {
-  it('should have the same key and value', () => {
-    _.each(EVENTS, (v, k) => expect(k).to.equal(v));
+  describe('events', () => {
+    it('should have the same key and value', () => {
+      _.each(EVENTS, (v, k) => expect(k).to.equal(v));
+    });
+
+    it('should have unique keys', () => {
+      expect(_.keys(EVENTS)).to.deep.equal(_.uniq(_.keys(EVENTS)));
+    });
   });
 
-  it('should have unique keys', () => {
-    expect(_.keys(EVENTS)).to.deep.equal(_.uniq(_.keys(EVENTS)));
+  describe('player states', () => {
+    it('should have the same key and value', () => {
+      _.each(PLAYER_STATES, (v, k) => expect(k).to.equal(v));
+    });
+
+    it('should have unique keys', () => {
+      expect(_.keys(PLAYER_STATES)).to.deep.equal(_.uniq(_.keys(PLAYER_STATES)));
+    });
   });
 });
--- src/state/constants.js
+++ src/state/constants.js
@@ -1,6 +1,33 @@
 /**
- * Define application event name constants
+ * Application event name constants
  */
 export const EVENTS = {
-  SAGA_INITIALIZED: 'SAGA_INITIALIZED',
+  LOAD_IFRAME: 'LOAD_IFRAME',
+  LOAD_IFRAME_DONE: 'LOAD_IFRAME_DONE',
+  LOAD_IFRAME_FAIL: 'LOAD_IFRAME_FAIL',
+  PLAY_COMMAND: 'PLAY_COMMAND',
+  PLAYING: 'PLAYING',
+  PAUSE_COMMAND: 'PAUSE_COMMAND',
+  PAUSING: 'PAUSING',
+  ON_EVENT: 'ON_EVENT',
+  NOOP: 'NOOP',
+};
+
+/**
+ * iFrame player names
+ */
+export const PLAYER_TARGETS = {
+  YOUTUBE: 'youtube',
+  SOUNDCLOUD: 'soundcloud',
+};
+
+/**
+ * Player state that represent the current state of the iframe players
+ */
+export const PLAYER_STATES = {
+  LOADING: 'LOADING',
+  LOADED: 'LOADED',
+  PLAYING: 'PLAYING',
+  PAUSED: 'PAUSED',
+  ENDED: 'ENDED',
 };
