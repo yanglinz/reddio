3b359cbd734b63d068947c973127d77f482a235a
Yanglin Zhao <yz1876@gmail.com>
2016-11-12 17:47:40 -0500

    Move analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import 'whatwg-fetch';
 import 'babel-polyfill';
-import 'analytics/segment';
-import 'analytics/sentry';
+import 'services/analytics/segment';
+import 'services/analytics/sentry';
 import './core/styles/main.scss';
 import { initialize } from './core/entry';
 
