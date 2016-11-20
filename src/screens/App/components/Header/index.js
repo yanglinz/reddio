ca433c84179c643f98112b396d1cb469cb1e63e2
Yanglin Zhao <yz1876@gmail.com>
2016-11-20 11:58:51 -0500

    Add polish to header

--- src/screens/App/components/Header/index.js
+++ src/screens/App/components/Header/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import InlineSVG from 'svg-inline-react';
 import { Link } from 'react-router';
+
+import logo from './logo.svg';
+import './index.scss';
 
 function Header() {
   return (
-    <div className="Header">
-      <h1>Header</h1>
-      <ul>
-        <li><Link to="/">home</Link></li>
-        <li><Link to="/r/listentothis">listentothis</Link></li>
-        <li><Link to="/r/trance">trance</Link></li>
-      </ul>
-    </div>
+    <header className="Header">
+      <nav className="Header-nav">
+        <div className="Header-brand">
+          <Link to="/">
+            <div className="Header-logo">
+              <InlineSVG src={logo} />
+            </div>
+            <h1 className="Header-siteName">
+              <span>/reddio.co</span>
+            </h1>
+          </Link>
+        </div>
+      </nav>
+
+      <div className="Header-callToAction">
+        <h2>
+          Reddio helps you find new music.
+          <br/>
+          Discover and listen to electronic music.
+        </h2>
+      </div>
+    </header>
   );
 }
 
