af3ef7845aee7b8f113bb5ba1cb91b31cab7bc21
Yanglin Zhao <yz1876@gmail.com>
2016-11-20 22:23:17 -0500

    Add home listings for subreddits and multireddits

--- src/screens/App/screens/Home/index.js
+++ src/screens/App/screens/Home/index.js
@@ -1,9 +1,72 @@
+import _ from 'lodash';
 import React from 'react';
+import { Link } from 'react-router';
+
+import { SUBREDDITS, MULTIREDDITS } from 'services/reddit-api/manifest';
+
+import './index.scss';
 
 function HomeScreen() {
+  const multiredditSection = (
+    <section className="HomeScreen-section">
+      <div className="HomeScreen-sectionHeader">
+        <h2 className="HomeScreen-sectionTitle">
+          Multireddits
+        </h2>
+        <p className="HomeScreen-sectionLabel">
+          Listen to music posted on a combination of subreddits.
+        </p>
+      </div>
+
+      <ul className="HomeScreen-sectionList">
+        {_.map(MULTIREDDITS, multireddit => (
+          <li className="HomeScreen-sectionItem">
+            <Link to={multireddit.pathname}>
+              <h3 className="HomeScreen-sectionItemTitle">
+                {multireddit.title}
+              </h3>
+              <p className="HomeScreen-sectionItemLabel">
+                {multireddit.description}
+              </p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+
+  const subredditSection = (
+    <section className="HomeScreen-section">
+      <div className="HomeScreen-sectionHeader">
+        <h2 className="HomeScreen-sectionTitle">
+          Subreddits
+        </h2>
+        <p className="HomeScreen-sectionLabel">
+          Listen to music posted on a specific subreddits.
+        </p>
+      </div>
+
+      <ul className="HomeScreen-sectionList">
+        {_.map(SUBREDDITS, subreddit => (
+          <li className="HomeScreen-sectionItem">
+            <Link to={subreddit.pathname}>
+              <h3 className="HomeScreen-sectionItemTitle">
+                {subreddit.title}
+              </h3>
+              <p className="HomeScreen-sectionItemLabel">
+                {subreddit.description}
+              </p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+
   return (
     <div className="HomeScreen">
-      <h1>HomeScreen</h1>
+      {multiredditSection}
+      {subredditSection}
     </div>
   );
 }
