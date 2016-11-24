81a7eb52c68fb565eb6ef45fa2f9deff0e68f1a5
Yanglin Zhao <yanglinz@users.noreply.github.com>
2016-11-24 14:13:30 -0500

    Add home listings for subreddits and multireddits (#123)

---
+++ src/services/reddit-api/manifest.js
@@ -0,0 +1,45 @@
+export const SUBREDDITS = {
+  LISTEN_TO_THIS: {
+    title: 'Listen to This',
+    description: 'A place to discover music by new or overlooked artists.',
+    pathname: '/r/listentothis',
+  },
+  HIP_HOP_HEADS: {
+    title: 'Hip-Hop Heads',
+    description: 'A subreddit dedicated to hip-hop news, discussion and music.',
+    pathname: '/r/hiphopheads',
+  },
+  ELECTRIC_MUSIC: {
+    title: 'Electric Music',
+    description: 'A subreddit dedicated to electric music and edm.',
+    pathname: '/r/electronicmusic',
+  },
+  METAL: {
+    title: 'Metal',
+    description: 'A subreddit focused on heavy metal songs.',
+    pathname: '/r/metal',
+  },
+};
+
+export const MULTIREDDITS = {
+  TRUE_MUSIC: {
+    title: 'True Music',
+    description: 'Reddit\'s most unique, interesting, and well moderated musical communities. ',
+    pathname: '/user/evilnight/m/truemusic',
+  },
+  THE_FIRE_HOSE: {
+    title: 'The Fire Hose',
+    description: 'Reddit\'s top 100 most active musical communities.',
+    pathname: '/user/evilnight/m/thefirehose',
+  },
+  THE_FOUNTAIN: {
+    title: 'The Fountain',
+    description: 'Reddit\'s 100 moderately active music communities.',
+    pathname: '/user/evilnight/m/thefountain',
+  },
+  THE_DRIP: {
+    title: 'The Drip',
+    description: 'Reddit\'s 100 least active music communities.',
+    pathname: '/user/evilnight/m/thedrip',
+  },
+};
